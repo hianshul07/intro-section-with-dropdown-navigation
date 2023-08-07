b63ca1192176e60242ad8bfbf390ad82fdf289f3
Anshul Kumar <hianshul07@gmail.com>
2023-08-07 22:57:45 +0530

    sidebar added

---
+++ script.js
@@ -0,0 +1,20 @@
+const showMenu = document.getElementById('menuIcon');
+const closeMenu = document.getElementById('closeIcon');
+const sideMenu = document.getElementById('aside');
+const mainScreen = document.getElementById('main');
+
+showMenu.addEventListener('click', function () {
+	sideMenu.style.display = 'block';
+
+	mainScreen.style.backgroundColor = 'rgba(0, 0, 0, 0.6)';
+	mainScreen.style.opacity = '1';
+});
+
+closeMenu.addEventListener('click', function () {
+	sideMenu.style.display = '';
+	mainScreen.style.opacity = '';
+	mainScreen.style.backgroundColor = '';
+});
+
+if (sideMenu.style.display === 'block') {
+}
