79330696b6e97e5565f938583125e7579dbf3414
Anshul Kumar <hianshul07@gmail.com>
2023-08-11 21:24:57 +0530

    almost done

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ const closeMenu = document.getElementById('closeIcon');
 const sideMenu = document.getElementById('aside');
 const mainScreen = document.getElementById('body');
 const clickable = document.querySelector('.click');
-const company = document.querySelector('#company');
+const clickable1 = document.querySelector('#company');
 const arrowDown = document.querySelector('.arrow-down');
 const arrowUp = document.querySelector('.arrow-up');
 const arrDown = document.querySelector('#arr-down');
 const arrUp = document.querySelector('#arr-up');
+const featuresMenu = document.querySelector('#menu-one')
 
 showMenu.addEventListener('click', function () {
 	sideMenu.style.display = 'block';
@@ -26,15 +27,17 @@ clickable.addEventListener('click', function () {
 	if (arrowDown.style.display === 'none') {
 		arrowUp.style.display = 'none';
 		arrowDown.style.display = 'block';
+		featuresMenu.style.display = '';
 		console.log('up');
 	} else {
+		featuresMenu.style.display = 'block';
 		arrowDown.style.display = 'none';
 		arrowUp.style.display = 'block';
 		console.log('down');
 	}
 });
 
-company.addEventListener('click', function () {
+clickable1.addEventListener('click', function () {
 	if (arrDown.style.display === 'none') {
 		arrUp.style.display = 'none';
 		arrDown.style.display = 'block';
